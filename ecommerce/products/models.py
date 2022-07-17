8ab300986431a0c932d3b5bcab58d6ab52530f89
Gustavo Lozada <gu.lozada9@gmail.com>
2022-07-17 14:48:51 -0400

    Create Type model and make migrations

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from ecommerce.utils.models import BaseModel
+
+
+class Type(BaseModel):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
