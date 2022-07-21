f9b6d5e15f13f90147e70d798ac11d9436387dcc
Gustavo Lozada <gu.lozada9@gmail.com>
2022-07-20 21:26:15 -0400

    Add category context to frontpage rendering

--- ecommerce/core/views/frontpage.py
+++ ecommerce/core/views/frontpage.py
@@ -1,8 +1,12 @@
 from django.views.generic import TemplateView
 
+from ecommerce.products.models import Category
+from ecommerce.products.serializers.category import CategorySerializer
+
 
 class FrontPageView(TemplateView):
     template_name = 'core/frontpage.html'
 
     def get(self, request, **kwargs):
-        return self.render_to_response({})
+        categories = CategorySerializer(Category.objects.all(), many=True).data
+        return self.render_to_response({'categories': categories})
