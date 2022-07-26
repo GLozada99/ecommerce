d16c1cdc8a5140525e0bbc7763dcc0f2fb91a305
Gustavo Lozada <gu.lozada9@gmail.com>
2022-07-26 15:14:43 -0400

    Implement get in ProductListView using ProductService and CategoryService

--- ecommerce/products/views/product.py
+++ ecommerce/products/views/product.py
@@ -1,12 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView  # type: ignore
 
-from ecommerce.products.models import Product
-from ecommerce.products.serializers.product import ProductSerializer
+from ecommerce.products.services.category import CategoryService
+from ecommerce.products.services.product import ProductService
 
 
 class ProductListView(TemplateView):
     template_name = 'products/list.html'
 
     def get(self, request, **kwargs):
-        products = ProductSerializer(Product.objects.all(), many=True).data
-        return self.render_to_response({'products': products})
+        current_category = CategoryService.get_current_category(
+            request.GET.get('category')
+        )
+        products = ProductService.get_products(request.GET.get('category'))
+        categories = CategoryService.get_short_categories()
+
+        return self.render_to_response(
+            {
+                'products': products,
+                'categories': categories,
+                'current_category': current_category
+            }
+        )
