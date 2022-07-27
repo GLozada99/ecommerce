e33cb0fb8ec5395aab7aa32f2978e5781e9f6eaf
Gustavo Lozada <gu.lozada9@gmail.com>
2022-07-26 23:21:10 -0400

    Create get_order_by_options method and update get_products to use order_by value

--- ecommerce/products/services/product.py
+++ ecommerce/products/services/product.py
@@ -5,11 +5,22 @@ from ecommerce.products.serializers.product import ProductSerializer
 class ProductService:
 
     @staticmethod
-    def get_products(current_category: str) -> list[dict | None]:
+    def get_products(
+            current_category: str,
+            order_by: str
+    ) -> list[dict | None]:
         """Returns product list of dicts representation based on
         current_category."""
         products = Product.objects.all()
         if current_category:
             products = products.filter(category__slug=current_category)
-
+        if order_by:
+            products = products.order_by(order_by)
         return ProductSerializer(products, many=True).data
+
+    @staticmethod
+    def get_order_by_options():
+        return [
+            {'field_name': 'name', 'show_name': 'Name'},
+            {'field_name': 'current_price', 'show_name': 'Price'},
+        ]
