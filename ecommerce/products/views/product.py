a320bde970d21ff8481beee70d1b5b5d267a674f
Gustavo Lozada <gu.lozada9@gmail.com>
2022-07-26 23:21:53 -0400

    Modify get in ProductListView to use order_by functionality

--- ecommerce/products/views/product.py
+++ ecommerce/products/views/product.py
@@ -8,16 +8,23 @@ class ProductListView(TemplateView):
     template_name = 'products/list.html'
 
     def get(self, request, **kwargs):
+        current_order_by = request.GET.get('order_by', '')
+        order_by_options = ProductService.get_order_by_options()
+
         current_category = CategoryService.get_current_category(
             request.GET.get('category')
         )
-        products = ProductService.get_products(request.GET.get('category'))
+        products = ProductService.get_products(
+            request.GET.get('category'), current_order_by
+        )
         categories = CategoryService.get_short_categories()
 
         return self.render_to_response(
             {
                 'products': products,
                 'categories': categories,
-                'current_category': current_category
+                'current_category': current_category,
+                'current_order_by': current_order_by,
+                'order_by_options': order_by_options,
             }
         )
