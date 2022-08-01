701399a0f5df982d167f58ade20135541cc548b9
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-01 19:48:42 -0400

    Create Client model and make migration

--- ecommerce/clients/models.py
+++ ecommerce/clients/models.py
@@ -1,2 +1,14 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from ecommerce.utils.models import SafeModel
+
+
+class Client(SafeModel):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+    authentication_provider = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}, {self.email}'
