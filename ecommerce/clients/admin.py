e1cc7737bced60e6d22f06703b9de4f0a020e39a
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-04 17:38:48 -0400

    Add Address model to admin

--- ecommerce/clients/admin.py
+++ ecommerce/clients/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from ecommerce.clients.models import Client
+from ecommerce.clients.models import Address, Client
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'email')
+    list_display = ('user', 'authentication_provider')
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('state', 'city', 'first_line', 'second_line')
