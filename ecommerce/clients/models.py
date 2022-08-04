508621e4756c8fb54dc195226a8834d7e5865bcd
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-04 17:36:53 -0400

    Create Address model and make migrations

--- ecommerce/clients/models.py
+++ ecommerce/clients/models.py
@@ -12,3 +12,21 @@ class Client(SafeModel):
 
     def __str__(self) -> str:
         return f'{self.user}\n{self.authentication_provider}'
+
+
+class Address(Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    state = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    first_line = models.CharField(
+        max_length=100, help_text='Sector, Street')
+    second_line = models.CharField(
+        max_length=100, help_text='Building, Apartment, Floor, House No.')
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+    def __str__(self) -> str:
+        return (f'{self.state}, {self.city}\n'
+                f'{self.first_line}\n'
+                f'{self.second_line}')
