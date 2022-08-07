0edd08d439d70f83773057e3ff07b8e65d0f2c83
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-06 23:13:02 -0400

    Set necessary settings for django allauth

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,6 +19,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     'safedelete',
     'thumbnails',
@@ -157,3 +163,14 @@ THUMBNAILS = {
 }
 
 AUTH_USER_MODEL = 'core.User'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/site/'
