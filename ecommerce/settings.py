be1f30c0917d1b060658cdc0f77cb46132682efe
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-12 19:26:28 -0400

    Add LOCALE_PATHS to settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -102,6 +102,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "staticfiles/"
