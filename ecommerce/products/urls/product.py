aa237f3e396c137c68bf5578e46df76b8e48c12d
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-14 20:41:08 -0400

    Add url for product detail

--- ecommerce/products/urls/product.py
+++ ecommerce/products/urls/product.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from ecommerce.products.views.product import ProductListView
+from ecommerce.products.views.product import ProductDetailView, ProductListView
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(),
          name='products-list'),
+    path('<slug:slug>/', ProductDetailView.as_view(),
+         name='products-detail'),
 ]
