18c619863ce349cc24e9227b9238354124bf38bb
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-16 16:18:37 -0400

    Modify createproducts command to use the Product and ProductConfiguration structure

--- ecommerce/products/management/commands/createproducts.py
+++ ecommerce/products/management/commands/createproducts.py
@@ -3,16 +3,14 @@ import os
 from django.core.files import File
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.template.defaultfilters import slugify
+from django.utils.text import slugify
 from faker import Faker
 
-from ecommerce.products.models.composite_models import (ProductConfiguration,
-                                                        ProductImage, )
+from ecommerce.products.models.composite_models import ProductConfiguration
 from ecommerce.products.models.models import Category, Product
 
 
 class Command(BaseCommand):
-
     help = ('A command to populate the product table.\n'
             'This command does not need parameters')
 
@@ -20,7 +18,7 @@ class Command(BaseCommand):
     def handle(self, *args: tuple, **kwargs: dict) -> None:
         self.stdout.write('Creating Products.')
         if Product.objects.exists():
-            self.stdout.write(self.style.SUCCESS('Categories already exist.'))
+            self.stdout.write(self.style.SUCCESS('Products already exist.'))
         else:
             faker = Faker()
             categories = {
@@ -28,31 +26,36 @@ class Command(BaseCommand):
                 for category in Category.objects.values()
             }
             path = './pics/products'
-            for filename in os.listdir(path):
-                f = os.path.join(path, filename)
-                name = filename.split('.')[0]
-                with open(f, 'rb') as fil:
-                    product = Product(
-                        name=name,
+            for directory in os.listdir(path):
+                full_directory = os.path.join(path, directory)
+                for sub_dir in os.listdir(full_directory):
+                    full_subdirectory = os.path.join(full_directory, sub_dir)
+                    product = Product.objects.create(
+                        name=sub_dir,
                         general_description='\n\r\n\r'.join(
                             faker.paragraphs(20)
                         ),
-                        slug=slugify(name),
-                        category_id=categories.get(name.split('_')[0])
+                        slug=slugify(sub_dir[:50]),
+                        category_id=categories.get(directory)
                     )
-                    product.general_image.save(f, File(fil))
-                    product.save()
-                type_product = ProductConfiguration.objects.create(
-                    product=product,
-                    name=faker.word(),
-                    description='\n\r\n\r'.join(faker.paragraphs(5)),
-                    current_price=faker.pydecimal(left_digits=5,
-                                                  right_digits=2,
-                                                  min_value=100),
-                )
-                with open(f, 'rb') as fil:
-                    ip = ProductImage(product=type_product)
-                    ip.image.save(f, File(fil))
-                    ip.save()
+
+                    for filename in os.listdir(full_subdirectory):
+                        name = filename.split('.')[0]
+                        full_filepath = os.path.join(
+                            full_subdirectory, filename
+                        )
+                        configuration = ProductConfiguration(
+                            product=product,
+                            name=name,
+                            slug=slugify(name[:50]),
+                            description='\n\r\n\r'.join(faker.paragraphs(5)),
+                            current_price=faker.pydecimal(left_digits=5,
+                                                          right_digits=2,
+                                                          min_value=100),
+                        )
+                        with open(full_filepath,
+                                  'rb') as fil:
+                            configuration.picture.save(name, File(fil))
+                            configuration.save()
 
             self.stdout.write(self.style.SUCCESS('OK'))
