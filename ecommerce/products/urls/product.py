8477dcedca2eadb83aae9afcea0f7c81fc63ae8b
Gustavo Lozada <gu.lozada9@gmail.com>
2022-08-20 21:28:27 -0400

    Add order_by/ to products route

--- ecommerce/products/urls/product.py
+++ ecommerce/products/urls/product.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from ecommerce.products.views.product.list import (ProductListView,
                                                    breadcrumb_view,
-                                                   category_selection_view, )
+                                                   category_selection_view,
+                                                   order_by_view, )
 
 app_name = 'products'
 urlpatterns = [
-    path('', ProductListView.as_view(template_name='list/list.html'),
+    path('', ProductListView.as_view(
+        template_name='list.html'),
          name='list'),
     path('search/', ProductListView.as_view(
         template_name='list/items_hx.html'),
@@ -15,4 +17,6 @@ urlpatterns = [
          name='current-category'),
     path('breadcrumb/', breadcrumb_view,
          name='breadcrumb'),
+    path('order_by/', order_by_view,
+         name='order_by'),
 ]
