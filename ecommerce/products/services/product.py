46a9c9e5b06dad71952de5753a79ca4dfe8f6b57
Gustavo Lozada <gu.lozada9@gmail.com>
2022-09-01 09:07:34 -0400

    Separate ProductService into list and detail services

--- ecommerce/products/services/product.py
+++ ecommerce/products/services/product.py
@@ -1,20 +1,64 @@
 from django.db.models import OuterRef, QuerySet, Subquery
 
 from ecommerce.products.models.composite_models import ProductConfiguration
+from ecommerce.products.models.models import Product
 
 
-class ProductService:
-
+class ProductListService:
     @classmethod
     def get_products(cls, queryset: QuerySet, order_by: str) -> QuerySet:
-        configurations = cls._get_configurations()
+        configurations_subquery = cls._get_configurations_subquery()
         return queryset.annotate(
             current_price=Subquery(
-                configurations.values('current_price')[:1]
-            )
-        ).order_by(order_by if order_by else 'name')
+                configurations_subquery.values('current_price')[:1]
+            ),
+        ).order_by(order_by if order_by else 'pk')
 
-    @classmethod
-    def _get_configurations(cls) -> QuerySet:
+    @staticmethod
+    def _get_configurations_subquery() -> QuerySet:
         return ProductConfiguration.objects.filter(
             product=OuterRef('pk')).order_by('pk')
+
+
+class ProductDetailService:
+    def __init__(self, product: Product):
+        self.product = product
+
+    def get_product_configuration(self, id: int) -> ProductConfiguration:
+        try:
+            configuration = self.product.configurations.get(id=id)
+        except ProductConfiguration.DoesNotExist:
+            configuration = self.product.configurations.first()
+        return configuration
+
+    def get_product_thumbnails(self, number: int = 5) -> list[dict[str, str]]:
+
+        configurations_thumbnail_data = [
+            {
+                'url': str(configurations.picture.thumbnails.small.url),
+                'id': str(configurations.id),
+                'type': 'configurations'
+            }
+            for configurations in self.product.configurations.all()
+        ]
+
+        general_thumbnail_data = [
+            {
+                'url': str(pictures.picture.thumbnails.small.url),
+                'id': str(pictures.id),
+                'type': 'pictures'
+            }
+            for pictures in self.product.pictures.all()[:number]
+        ]
+
+        return general_thumbnail_data + configurations_thumbnail_data
+
+    def get_configurations_data(self) -> list[dict[str, str | int]]:
+        return [
+            {
+                'id': data.id,
+                'name': data.name,
+                'current_price': str(data.current_price),
+                'url': data.picture.thumbnails.small.url,
+            } for data in self.product.configurations.all()
+        ]
