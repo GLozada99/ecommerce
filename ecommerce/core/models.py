2ca7aa72b2f899beb173f938778def27052ed907
Gustavo Lozada <gu.lozada9@gmail.com>
2022-09-05 21:46:17 -0400

    Create SlideImage model en core app and make migrations

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from thumbnails.fields import ImageField
 
 from ecommerce.utils.models import BaseModel
 
@@ -7,3 +9,11 @@ class User(AbstractUser, BaseModel):
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}, {self.email}'
+
+
+class SlideImage(BaseModel):
+    text = models.CharField(max_length=60)
+    icon = ImageField(upload_to='slide/', pregenerated_sizes=['slide'])
+
+    def frontpage_picture_url(self) -> str:
+        return self.icon.thumbnails.slide.url
