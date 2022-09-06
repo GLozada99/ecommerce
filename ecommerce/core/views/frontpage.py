5046014dbc49d84d7764fb748431fda0afd10cf3
Gustavo Lozada <gu.lozada9@gmail.com>
2022-09-05 21:29:38 -0400

    Update frontpage template and view to use images fron staticfiles on slides

--- ecommerce/core/views/frontpage.py
+++ ecommerce/core/views/frontpage.py
@@ -3,6 +3,7 @@ from typing import Mapping
 from django.views.generic import TemplateView
 
 from ecommerce.products.services.category import CategoryService
+from ecommerce.products.services.product import ProductListService
 
 
 class FrontPageView(TemplateView):
@@ -10,5 +11,13 @@ class FrontPageView(TemplateView):
 
     def get_context_data(self, **kwargs: dict) -> Mapping:
         context = super(FrontPageView, self).get_context_data(**kwargs)
+        product_limit = 8
         context['categories'] = CategoryService.get_categories()
+        context['products'] = (ProductListService.
+                               get_random_products(product_limit))
+        context['slides'] = [
+            {'url': 'front1.png', 'text': 'Shopping Day'},
+            {'url': 'front2.png', 'text': 'Summer Sale'},
+            {'url': 'front3.png', 'text': 'Back to School'},
+        ]
         return context
