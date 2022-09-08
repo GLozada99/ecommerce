4d44b20e8e4a9920768cc38fb7836c669005a419
Gustavo Lozada <gu.lozada9@gmail.com>
2022-09-08 09:20:44 -0400

    Create FrontPageService and use it in FrontPageView

---
+++ ecommerce/core/services.py
@@ -0,0 +1,22 @@
+from django.db.models import QuerySet
+
+from ecommerce.core.models import SlideImage
+from ecommerce.products.services.category import CategoryService
+from ecommerce.products.services.product import ProductListService
+
+
+class FrontPageService:
+
+    @staticmethod
+    def get_slides(slide_limit: int) -> QuerySet:
+        slides = SlideImage.objects.all().order_by('-pk')
+        slide_total = slides.count()
+        return slides[:min(slide_limit, slide_total)]
+
+    @classmethod
+    def get_context(cls, slide_limit: int, product_limit: int) -> dict:
+        return {
+            'slides': cls.get_slides(slide_limit),
+            'products': ProductListService.get_random_products(product_limit),
+            'categories': CategoryService.get_categories(),
+        }
--- ecommerce/core/views/frontpage.py
+++ ecommerce/core/views/frontpage.py
@@ -2,8 +2,7 @@ from typing import Mapping
 
 from django.views.generic import TemplateView
 
-from ecommerce.products.services.category import CategoryService
-from ecommerce.products.services.product import ProductListService
+from ecommerce.core.services import FrontPageService
 
 
 class FrontPageView(TemplateView):
@@ -11,13 +10,6 @@ class FrontPageView(TemplateView):
 
     def get_context_data(self, **kwargs: dict) -> Mapping:
         context = super(FrontPageView, self).get_context_data(**kwargs)
-        product_limit = 8
-        context['categories'] = CategoryService.get_categories()
-        context['products'] = (ProductListService.
-                               get_random_products(product_limit))
-        context['slides'] = [
-            {'url': 'front1.png', 'text': 'Shopping Day'},
-            {'url': 'front2.png', 'text': 'Summer Sale'},
-            {'url': 'front3.png', 'text': 'Back to School'},
-        ]
+        context |= FrontPageService.get_context(slide_limit=3,
+                                                     product_limit=8)
         return context
