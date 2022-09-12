2c098a787a338ca16ea6c0f75ed4a242600dc7b6
Gustavo Lozada <gu.lozada9@gmail.com>
2022-09-12 16:06:35 -0400

    Create Cart and CartProducts model

--- ecommerce/order/models.py
+++ ecommerce/order/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from ecommerce.core.models import User
+from ecommerce.products.models.composite_models import ProductConfiguration
+from ecommerce.utils.models import BaseModel
+
+
+class Cart(BaseModel):
+    products = models.ManyToManyField(ProductConfiguration,
+                                      through='CartProducts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CartProducts(BaseModel):
+    product = models.ForeignKey(ProductConfiguration, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['product', 'cart'],
+                name='product_card_unique_constraint',
+            ),
+        ]
