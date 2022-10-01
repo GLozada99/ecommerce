3ccfa628e6109fde7ad8c2a59a2a2fe17acae77d
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-01 12:30:14 -0400

    Add get_cart_view to urls

--- ecommerce/order/urls/cart.py
+++ ecommerce/order/urls/cart.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from ecommerce.order.views.cart import ManageCartView
+from ecommerce.order.views.cart import ManageCartView, get_cart_view
 
 app_name = 'order'
 urlpatterns = [
     path('manage/<int:product_id>', ManageCartView.as_view(),
          name='manage'),
+    path('', get_cart_view,
+         name='show'),
 ]
