0e9db72c96522fa76db1b515f2ec5d69468f0274
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-03 13:48:06 -0400

    Update MiniCartView to use CartViewActionMixin

--- ecommerce/order/views/minicart.py
+++ ecommerce/order/views/minicart.py
@@ -4,17 +4,14 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from ecommerce.order.mixins import CartViewActionMixin
 from ecommerce.order.services.cart import CartService
 from ecommerce.products.services.product import ProductListService
 
 
-class MiniCartView(TemplateView):
+class MiniCartView(TemplateView, CartViewActionMixin):
     template_name = 'base/modals/minicart.html'
     cart_functions: dict[str, Callable[[CartService, int], None]] = {
-        'increase': lambda service, product_id: service.add_product(
-            product_id),
-        'decrease': lambda service, product_id: service.remove_product(
-            product_id),
         'delete': lambda service, product_id: service.delete_product(
             product_id),
         '_': lambda _, __: None,
@@ -22,24 +19,13 @@ class MiniCartView(TemplateView):
 
     def patch(self, request: HttpRequest,
               *args: Any, **kwargs: Any) -> HttpResponse:
-        service = CartService(self.request.user,
-                              self.request.COOKIES.get('cookie_id', ''))
-        action = self.request.GET.get('action', '_')
-        product_id = int(self.request.GET.get('product_id', 0))
-        self.cart_functions[action](service, product_id)
-
+        self.execute_cart_action()
         response = super().get(request, *args, **kwargs)
         return response
 
     def get_context_data(self, **kwargs: Any) -> Mapping:
-        service = CartService(self.request.user,
-                              self.request.COOKIES.get('cookie_id', ''))
-
-        context = super().get_context_data(**kwargs)
-
-        context |= service.get_cart_context(product_limit=5)
-
-        return context
+        return self.get_minicart_context(super().get_context_data(**kwargs),
+                                         product_limit=5)
 
 
 def add_cart_product_view(
