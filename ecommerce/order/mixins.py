2d7786efdca32da07979bb409e4d773e41676652
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-03 13:39:51 -0400

    Create mixins module and implement CartViewActionMixin

---
+++ ecommerce/order/mixins.py
@@ -0,0 +1,34 @@
+from typing import Callable, Mapping
+
+from django.http import HttpRequest
+
+from ecommerce.order.services.cart import CartService
+
+
+class CartViewActionMixin:
+    request: HttpRequest
+    cart_functions: dict[str, Callable[[CartService, int], None]]
+
+    def execute_cart_action(self) -> None:
+        service = CartService(self.request.user,
+                              self.request.COOKIES.get('cookie_id', ''))
+        action = self.request.GET.get('action', '_')
+        product_id = int(self.request.GET.get('product_id', 0))
+        self.cart_functions[action](service, product_id)
+
+    def get_minicart_context(self, context: Mapping,
+                             product_limit: int) -> Mapping:
+        service = CartService(self.request.user,
+                              self.request.COOKIES.get('cookie_id', ''))
+
+        context |= service.get_cart_context(product_limit=product_limit)
+
+        return context
+
+    def get_full_cart_context(self, context: Mapping) -> Mapping:
+        service = CartService(self.request.user,
+                              self.request.COOKIES.get('cookie_id', ''))
+
+        context |= service.get_full_cart_context()
+
+        return context
