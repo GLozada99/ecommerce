711db069f2322008f1aa0531b628582772600a97
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-03 12:34:36 -0400

    Add url for CartView

--- ecommerce/order/urls/cart.py
+++ ecommerce/order/urls/cart.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from ecommerce.order.views.cart import CartView
 from ecommerce.order.views.minicart import (MiniCartView,
                                             add_cart_product_view, )
 
 app_name = 'order'
 urlpatterns = [
+    path('/', CartView.as_view(),
+         name='full'),
     path('/manage', MiniCartView.as_view(),
          name='manage'),
     path('add/<int:product_id>', add_cart_product_view,
