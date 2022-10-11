960767eddeab691672e4a1fdd57f9c77449dd114
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-11 19:50:38 -0400

    Execute migrations

--- ecommerce/order/admin.py
+++ ecommerce/order/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from ecommerce.order.models import Cart
+from ecommerce.order.models.cart import Cart
+from ecommerce.order.models.order import Order
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ['user']
+    list_display = ['user', 'cookie_id']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'client']
