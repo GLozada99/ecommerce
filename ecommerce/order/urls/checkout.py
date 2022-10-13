de5f0c31c609dea506dd449e881b130739bd8d77
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-13 12:05:11 -0400

    Update cities keys

--- ecommerce/order/urls/checkout.py
+++ ecommerce/order/urls/checkout.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from ecommerce.order.views.checkout import CheckoutView, order_form_view
+from ecommerce.order.views.checkout import (CheckoutView, order_form_view,
+                                            order_submit_view, )
 
 app_name = 'order'
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
          name='checkout'),
     path('delivery/<int:delivery>', order_form_view,
          name='delivery'),
+    path('submit', order_submit_view,
+         name='submit'),
 ]
