3e6ee79f357d881d7f42bcaf43f56ea396fe1f4d
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-20 19:27:25 -0400

    Implement AddresService in clients app

--- ecommerce/order/services/checkout.py
+++ ecommerce/order/services/checkout.py
@@ -1,27 +1,16 @@
-import json
+from django.http import HttpRequest
 
 from django.http import HttpRequest
-from django.utils.translation import gettext as _
 
 from ecommerce.clients.models import Client
+from ecommerce.core.models import User
+from ecommerce.order.models.order import Order
 
 
 class CheckoutService:
 
     def __init__(self, request: HttpRequest):
         self.request = request
-
-    @staticmethod
-    def get_states() -> list:
-        with open('./states/states.json') as fil:
-            data = json.load(fil)
-            return data['data']
-
-    @staticmethod
-    def get_cities(state_id: int) -> list:
-        with open(f'./states/cities/cities_{state_id}.json') as fil:
-            data = json.load(fil)
-            return data['data']
 
     def get_client_profile(self) -> Client:
         try:
@@ -30,14 +19,11 @@ class CheckoutService:
             return Client.objects.create(user=self.request.user)
 
     def get_checkout_info_context(self) -> dict:
+        user: User = self.request.user
         return {
             'addresses': self.get_client_profile().addresses.all(),
-            'phone': self.request.user.phone,
-            'email': self.request.user.email,
-            'payment_types': [
-                _('Cash on delivery'),
-                _('Card on delivery'),
-                _('Bank Transfer on delivery'),
-            ],
-            'delivery': 0,
+            'phone': user.phone or '',
+            'email': user.email or '',
+            'payment_types': Order.payment_choices,
+            'delivery': 1,
         }
