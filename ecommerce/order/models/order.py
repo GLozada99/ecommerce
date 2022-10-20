9376622710e1eea95e4eafcbe6bfbef5fb458464
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-20 19:19:11 -0400

    Add payment_type and completed fields on Order model and make migrations

--- ecommerce/order/models/order.py
+++ ecommerce/order/models/order.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext as _
 
 from ecommerce.clients.models import Client
 from ecommerce.core.models import User
@@ -8,10 +9,31 @@ from ecommerce.utils.models import BaseModel
 
 
 class Order(BaseModel):
+    CASH_DELIVERY = _('Cash on delivery')
+    CARD_DELIVERY = _('Card on delivery')
+    BANK_TRANSFER_DELIVERY = _('Bank Transfer on delivery')
+    PICKUP = _('Payment on pick up')
+    PAYMENT_CHOICES = [
+        ('Cash', CASH_DELIVERY),
+        ('Card', CARD_DELIVERY),
+        ('Transfer', BANK_TRANSFER_DELIVERY),
+        ('Pickup', PICKUP)
+    ]
+
+    payment_type = models.CharField(max_length=8, choices=PAYMENT_CHOICES)
     employee = models.ForeignKey(User, on_delete=models.PROTECT)
     client = models.ForeignKey(Client, on_delete=models.PROTECT)
     products = models.ManyToManyField(ProductConfiguration,
                                       through='OrderProducts')
+    completed = models.BooleanField(default=False)
+
+    @classmethod
+    @property
+    def payment_choices(cls) -> list[str]:
+        return [choice[1] for choice in cls.PAYMENT_CHOICES]
+
+    def needs_delivery(self) -> bool:
+        return self.payment_type != self.PICKUP
 
 
 class OrderProducts(BaseModel):
