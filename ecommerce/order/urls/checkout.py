e1e779d93334334f549a7b78218c5d5b7b5374b0
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-20 19:34:03 -0400

    Set urls for add_addres_form, add_address, get_cities and order_submit views

--- ecommerce/order/urls/checkout.py
+++ ecommerce/order/urls/checkout.py
@@ -1,14 +1,23 @@
 from django.urls import path
 
-from ecommerce.order.views.checkout import (CheckoutView, order_form_view,
+from ecommerce.order.views.checkout import (CheckoutView,
+                                            add_address_form_view,
+                                            add_address_view, get_cities_view,
+                                            order_form_view,
                                             order_submit_view, )
 
 app_name = 'order'
 urlpatterns = [
     path('', CheckoutView.as_view(),
          name='checkout'),
-    path('delivery/<int:delivery>', order_form_view,
+    path('delivery', order_form_view,
          name='delivery'),
+    path('add-address-form', add_address_form_view,
+         name='add-address-form'),
+    path('add-address', add_address_view,
+         name='add-address'),
+    path('get-cities', get_cities_view,
+         name='get-cities'),
     path('submit', order_submit_view,
          name='submit'),
 ]
