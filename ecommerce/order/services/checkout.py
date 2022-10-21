5e79119058cffdb74da19cd12c4eee2056b3f072
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-20 22:31:26 -0400

    Implement CheckoutService.create_order method

--- ecommerce/order/services/checkout.py
+++ ecommerce/order/services/checkout.py
@@ -1,10 +1,11 @@
-from django.http import HttpRequest
-
+from django.db.transaction import atomic
 from django.http import HttpRequest
 
 from ecommerce.clients.models import Client
 from ecommerce.core.models import User
-from ecommerce.order.models.order import Order
+from ecommerce.core.services.mail import MailService
+from ecommerce.order.models.order import Order, OrderProducts
+from ecommerce.order.services.cart import CartInfoService, CartService
 
 
 class CheckoutService:
@@ -27,3 +28,39 @@ class CheckoutService:
             'payment_types': Order.payment_choices,
             'delivery': 1,
         }
+
+    @atomic  # type: ignore
+    def create_order(self) -> None:
+        # TODO: Breakdown method and use messages if cart is empty (create
+        #  custom exeption)
+        #  Implement mail templates on the simplest way
+        cart_service = CartService(self.request.user, '')
+        post_data = self.request.POST.dict()
+        client = self.get_client_profile()
+        client.user.phone = post_data.get('cellphone')
+        client.user.email = post_data.get('email')
+        data = {
+            'employee': self._get_employee(),
+            'client': client,
+            'payment_type': (Order.PaymentChoices.PICKUP
+                             if not post_data.get('delivery') else
+                             Order.PaymentChoices.get_choice(
+                                 post_data.get('payment'))).value,
+            'info': post_data.get('info')
+        }
+
+        order = Order.objects.create(**data)
+        order_products = [OrderProducts(
+            product=cart_product.product,
+            order=order,
+            quantity=cart_product.quantity,
+            price=cart_product.price
+        ) for cart_product in CartInfoService.get_product_data(
+            cart_service.cart)]
+        OrderProducts.objects.bulk_create(order_products)
+        cart_service.delete_all()
+        MailService.send_order_mails(order)
+
+    @staticmethod
+    def _get_employee() -> User:
+        return User.objects.filter(is_staff=True, is_active=True).first()
