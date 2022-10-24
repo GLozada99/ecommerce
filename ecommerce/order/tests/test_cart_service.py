40c131aebce386f7b0b2f7b843772cefd0b70a41
Gustavo Lozada <gu.lozada9@gmail.com>
2022-10-23 20:46:28 -0400

    Move helper functions to helpers.tests

--- ecommerce/order/tests/test_cart_service.py
+++ ecommerce/order/tests/test_cart_service.py
@@ -8,38 +8,15 @@ from model_bakery import baker
 from parameterized import parameterized
 
 from ecommerce.core.models import User
+from ecommerce.order.helpers.tests import (
+    get_random_cookie_id,
+    helper_test_add_product,
+    helper_test_delete_all_products,
+    helper_test_remove_product,
+)
 from ecommerce.order.models.cart import Cart, CartProducts
 from ecommerce.order.services.cart import CartService
 from ecommerce.products.models.composite_models import ProductConfiguration
-
-
-def _get_random_cookie_id() -> uuid.UUID:
-    return Cart.objects.filter(
-        cookie_id__isnull=False).order_by('?').first().cookie_id
-
-
-def _helper_test_add_product(
-        service: CartService, product_id: int, qty: int) -> None:
-    for _ in range(qty):
-        service.add_product(product_id)
-
-
-def _helper_test_remove_product(
-        service: CartService, product_id: int, qty: int,
-        qty_rem: int) -> None:
-    for _ in range(qty):
-        service.add_product(product_id)
-
-    for _ in range(qty_rem):
-        service.remove_product(product_id)
-
-
-def _helper_test_delete_all_products(
-        service: CartService, product_id: int, qty: int) -> None:
-    for _ in range(qty):
-        service.add_product(product_id)
-
-    service.delete_all()
 
 
 class CartServiceTestCase(TestCase):
@@ -51,7 +28,7 @@ class CartServiceTestCase(TestCase):
         baker.make(Cart, user=user)
 
     @parameterized.expand([  # type: ignore
-        ("old cookie id", str(_get_random_cookie_id())),
+        ("old cookie id", str(get_random_cookie_id())),
         ("new cookie id", str(uuid.uuid4())),
     ])
     def test_get_cart_anonymous_user(
@@ -62,7 +39,7 @@ class CartServiceTestCase(TestCase):
         self.assertIsNone(cart.user)
 
     @parameterized.expand([  # type: ignore
-        ("old cookie id", str(_get_random_cookie_id())),
+        ("old cookie id", str(get_random_cookie_id())),
         ("new cookie id", str(uuid.uuid4())),
     ])
     def test_get_cart_user(
@@ -75,17 +52,17 @@ class CartServiceTestCase(TestCase):
 
     @parameterized.expand([  # type: ignore
         ("add product",
-         partial(_helper_test_add_product, qty=5), 5,
+         partial(helper_test_add_product, qty=5), 5,
          lambda cart, product_id, qty: CartProducts.objects.get(
              cart=cart, product_id=product_id).quantity == qty),
 
         ("remove product",
-         partial(_helper_test_remove_product, qty=5, qty_rem=3), 2,
+         partial(helper_test_remove_product, qty=5, qty_rem=3), 2,
          lambda cart, product_id, qty: CartProducts.objects.get(
              cart=cart, product_id=product_id).quantity == qty),
 
         ("delete all products",
-         partial(_helper_test_delete_all_products, qty=5), 0,
+         partial(helper_test_delete_all_products, qty=5), 0,
          lambda cart, _, __: not cart.has_products)
     ])
     def test_manage_cart(self, _: str, helper: Callable, final_qty: int,
