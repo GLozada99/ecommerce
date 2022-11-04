882c58b139ccc8098fb01a9dfa861ea3e8c66b90
Gustavo Lozada <gu.lozada9@gmail.com>
2022-11-04 11:05:13 -0400

    Implement sendemail commmand

---
+++ ecommerce/core/management/commands/sendemail.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand, CommandParser
+
+
+class Command(BaseCommand):
+    help = ('A command to populate the slide_image table.\n'
+            'This command does not need parameters')
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument('emails', nargs='+', type=str)
+
+    def handle(self, *args: Any, **kwargs: Any) -> None:
+
+        emails = kwargs['emails']
+        try:
+            send_mail(
+                'This is a test subject',
+                'This is a test',
+                None,
+                emails,
+                fail_silently=False,
+            )
+        except ValueError:
+            self.stdout.write(self.style.ERROR('ONE OR MORE OF THE EMAILS '
+                                               'ARE NOT VALID'))
+            return
+
+        self.stdout.write(self.style.SUCCESS('OK'))
