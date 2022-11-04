a87ec88ef4875fe666dbe28d396a8e9f03d0cb08
Gustavo Lozada <gu.lozada9@gmail.com>
2022-11-04 11:49:36 -0400

    Implement basic addgoogleauth command

---
+++ ecommerce/core/management/commands/addgoogleauth.py
@@ -0,0 +1,26 @@
+from allauth.socialaccount.models import SocialApp
+from django.contrib.sites.models import Site
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from ecommerce.settings import env_settings
+
+
+class Command(BaseCommand):
+    help = ('A command to populate the slide_image table.\n'
+            'This command does not need parameters')
+
+    @transaction.atomic  # type: ignore
+    def handle(self, *args: tuple, **kwargs: dict) -> None:
+        if SocialApp.objects.filter(provider='google').exists():
+            self.stdout.write(self.style.SUCCESS('Google auth already '
+                                                 'implemented.'))
+            return
+        site = Site(domain=env_settings.SITE_DOMAIN,
+                    name=env_settings.SITE_NAME)
+        site.save()
+        app = SocialApp(provider='google', name='Google',
+                        client_id=env_settings.SITE_GOOGLE_ID,
+                        secret=env_settings.SITE_GOOGLE_SECRET)
+        app.save()
+        app.sites.add(site)
