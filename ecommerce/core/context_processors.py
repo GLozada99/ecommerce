f7db91f1c20a03355628e5ad4b479c72c2d0a75a
Gustavo Lozada <gu.lozada9@gmail.com>
2022-11-11 16:06:45 -0400

    remove context processor for yaml setings

--- ecommerce/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from typing import Mapping
-
-from django.core.handlers.wsgi import WSGIRequest
-
-from ecommerce.core.readers import YAMLReader
-
-
-def contact_info_processor(request: WSGIRequest) -> Mapping:
-    return {'contact_info': YAMLReader.get_contact_info()['contact-info']}
